3c048969187d1da94ac599cec75bd79be25e1800
naflorezr <naflorezr@gmail.com>
2021-09-15 18:50:59 -0500

    TermalesActivity

---
+++ app/src/main/java/com/example/colombiadiversa/TermalesActivity.kt
@@ -0,0 +1,11 @@
+package com.example.colombiadiversa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TermalesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_termales)
+    }
+}
